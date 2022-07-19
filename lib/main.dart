637f69f1771fbaaac05c02163bb70a480572fdc5
Nyan Lin Htet <nyanlinhtet206@gmail.com>
2022-07-19 16:47:28 +0630

    earn_about_image

--- lib/main.dart
+++ lib/main.dart
@@ -2,54 +2,63 @@ import 'package:flutter/material.dart';
 //import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Nyan First Apk'),
-          leading: const FlutterLogo(),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('Setting')));
-                },
-                icon: const Icon(Icons.settings)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.message)),
-          ],
-          backgroundColor: Colors.black,
-          toolbarHeight: 100,
-          elevation: 30,
-          shadowColor: Colors.green,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30))),
-        ),
-        body: Container(
-          color: Colors.red,
-          // width: 600,
-          // height: 200,
-          margin: const EdgeInsets.all(20),
-          child: Text(
-            'container',
-            style: TextStyle(fontSize: 20, color: Colors.white),
-          ),
-          alignment: Alignment.center,
-          constraints: const BoxConstraints(minWidth: 300, maxHeight: 400),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.home),
-          onPressed: () {
-            print('Click');
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Its Me'),
+        leading: const FlutterLogo(),
+        actions: [
+          const MyStatelessWidget(),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.message)),
+        ],
+        backgroundColor: Colors.black,
+        toolbarHeight: 80,
+        elevation: 30,
+        shadowColor: Colors.green,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))),
       ),
-      debugShowCheckedModeBanner: false,
+      body: Column(children: [
+        Container(
+            alignment: Alignment.center,
+            width: 300,
+            height: 300,
+            child: Image.network(
+              "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2022-06/220617-lightyear-mjf-1648-dd3fa6.jpg",
+              loadingBuilder: (_, widget, progress) {
+                if (progress == null) return widget;
+                return CircularProgressIndicator();
+              },
+            ))
+      ]),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('A SnackBar has been shown.'),
+          ),
+        );
+      },
+      child: const Icon(Icons.settings),
     );
   }
 }
