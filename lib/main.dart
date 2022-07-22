631c418378ecdf1bcfc464b4b03b31d068ba474b
Nyan Lin Htet <nyanlinhtet206@gmail.com>
2022-07-22 11:07:14 +0630

    input_field

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,56 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _information = "";
+  TextEditingController _name = TextEditingController();
+  TextEditingController _age = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Its Me'),
-        leading: const FlutterLogo(),
-        actions: [
-          const MyStatelessWidget(),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.message)),
-        ],
-        backgroundColor: Colors.black,
-        toolbarHeight: 80,
-        elevation: 30,
-        shadowColor: Colors.green,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))),
-      ),
-      body: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (BuildContext context, int index) {
-          return const Text("loream");
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Its Me'),
+          leading: const FlutterLogo(),
+          actions: [
+            const MyStatelessWidget(),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.message)),
+          ],
+          backgroundColor: Colors.black,
+          toolbarHeight: 80,
+          elevation: 30,
+          shadowColor: Colors.green,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: _name,
+            ),
+            TextField(
+              controller: _age,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _information = "Name is ${_name.text} Age is ${_age.text}";
+                  });
+                },
+                child: Text('Save')),
+            Text(
+              "Information is ${_information} ",
+              style: TextStyle(fontSize: 30),
+            )
+          ],
+        ));
   }
 }
 
